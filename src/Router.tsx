49fd39510d045454e35fd912e4af7e5bb1c8e634
Cj-K-7 <kllt7059@gmail.com>
2022-02-22 14:36:17 +0900

    set Basename of BrowserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,7 +7,7 @@ import TV from "./Routes/TV";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
         <Route path="/movie/*" element={<Movie />} />
