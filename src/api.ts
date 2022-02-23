319404aa5efaae2d0f3e2f092cb1415b8596a00f
Cj-K-7 <kllt7059@gmail.com>
2022-02-23 15:15:37 +0900

    hide API key

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const APIKey = "4161cf6d910bb2a80ad0681aa12722e";
+const APIKey = process.env.REACT_APP_MOVIEAPI_KEY;
 const basicURL = "https://api.themoviedb.org/3/";
 
 export interface IResult {
